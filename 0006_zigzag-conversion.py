00f607f1dfae5efaf337050a08532896e11d25f5
tsandrini <tomas.sandrini@seznam.cz>
2023-12-06 15:54:11 +0100

    feat(06-zigzag-conversion): complete python version

--- 0006_zigzag-conversion.py
+++ 0006_zigzag-conversion.py
@@ -1,3 +1,18 @@
+from typing import List
+from itertools import chain, cycle
+
+
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        return ""
+        if numRows == 1 or len(s) < 2:
+            return s
+        rows: List[List[str]] = [[] for _ in range(numRows)]
+        out: str = ""
+        rows_idxs = cycle(chain(range(0, numRows - 1, 1), range(numRows - 1, 0, -1)))
+        for char in s:
+            rows[next(rows_idxs)].append(char)
+
+        for row in rows:
+            out += "".join(row)
+
+        return out
